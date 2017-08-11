287aa3015044e16a0f5e8aab685c4deb27fbffe9
Mark Powers <markppowers0@gmail.com>
2017-08-11 17:37:01 -0400

    Finish some RPG stuff

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ import random
 import os
 import re
 
-bot_dir = "/root/cafe-mod-bot"#os.path.abspath('')
+bot_dir = os.path.abspath('')
+bot_username = ""
 
 # Set token from file
 token = ""
@@ -16,12 +17,65 @@ except:
 
 url = 'https://api.telegram.org/bot%s/' % token
 
+username_pattern = re.compile("@(\w+)")
+
 # Set last_update from file
 last_update = 0
 try:
 	last_update = int(open(bot_dir+"/last_update").read())
 except:
 	pass
+
+
+class Roll:
+	def __init__(self, init_string):
+		parts = init_string.split("d")
+		self.num = int(parts[0])
+		if(self.num < 1):
+			self.num = 1
+		self.size = int(parts[1])
+		if(self.size < 1):
+			self.size = 1
+		self.rolls = []
+		self.roll()
+
+	def roll(self):
+		if self.rolls:
+			return self.rolls
+		for i in range(self.num):
+			self.rolls.append(random.randint(1, self.size))
+		return self.rolls
+
+	def sum(self):
+		return sum(self.rolls)
+
+class Character:
+	def __init__(self, name):
+		self.name = name
+		self.inventory = {}
+		self.stats = {}
+
+	def give_item(self, item, amount, desc):
+		if item in self.inventory:
+			self.inventory[item][1] += amount
+		else:
+			self.inventory[item] = (desc, amount)
+	
+	def set_stat(self, stat, num):
+		self.stats[stat] = num
+
+	def stat_string(self):
+		result = self.name + ":"
+		for stat in self.stats.keys():
+			result += "\n" + stat + ": " + str(self.stats[stat])
+		return result
+	
+	def inventory_string(self):
+		result = self.name + ":"
+		for item in self.inventory.keys():
+			result += "\n" + item + " (x" + str(self.inventory[item][1]) + ") - " + self.inventory[item][0]
+		return result
+
 
 # Parse existing triggers
 phrases = []
@@ -45,11 +99,27 @@ for file in os.listdir(bot_dir+"/pasta"):
 print("\nPASTA:")
 print(list(pasta.keys()))
 
+
 characters = {}
 for file in os.listdir(bot_dir+"/characters"):
-	if file.endswith(".txt."):
-		with open(bot_dir+"/pasta/"+file, 'r') as f:
-			
+	if file.endswith(".txt"):
+		with open(bot_dir+"/characters/"+file, 'r') as f:
+			lines = f.readlines()
+			char = Character(lines.pop(0).strip())
+			for line in lines:
+				#line = line.strip()
+				if line.startswith("inventory:"):
+					line = line.replace("inventory:","",1)
+					for item in line.split(":"):
+						group = item.split(",")
+						char.give_item(group[0], int(group[1]), group[2])
+				elif "=" in line:
+					parts = line.split("=")
+					char.set_stat(parts[0], int(parts[1]))					
+			characters[file[:-4]] = char
+
+print("\nCHARACTERS:")
+print(list(characters.keys()))	
 
 def addTrigger(parts):
 	if(len(parts) > 1):
@@ -66,7 +136,6 @@ def addPasta(parts):
 		file_name = parts[0].replace(" ", "-") + ".txt"
 		with open(bot_dir+"/pasta/"+file_name, 'w') as f:
 			f.write(parts[1]+"\n")
-
 
 def findWord(w, p):
 	w = w.lower()
@@ -87,30 +156,7 @@ def sendPasta(message, id):
 		sendMessage(id, pasta[message])
 	else:
 		sendMessage(id, pasta[random.choice(list(pasta.keys()))])
-
-class Roll:
-	def __init__(self, init_string):
-		parts = init_string.split("d")
-		self.num = int(parts[0])
-		if(self.num < 1):
-			self.num = 1
-		self.size = int(parts[1])
-		if(self.size < 1):
-			self.size = 1
-		self.rolls = []
-		self.roll()
-
-	def roll(self):
-		if self.rolls:
-			return self.rolls
-		for i in range(self.num):
-			self.rolls.append(random.randint(1, self.size))
-		return self.rolls
-
-	def sum(self):
-		return sum(self.rolls)
-		
-
+	
 def rollDice(message, id):
 	message = message.replace("/roll ","",1)
 	message = message.replace("/r ","",1)
@@ -132,24 +178,32 @@ def rollDice(message, id):
 	result += "\n = " + str(total)
 	sendMessage(id, result)
 
-class Character:
-	def __init__(self, name):
-		self.name = name
-		self.inventory = {}
-		self.stats = {}
+def show_stats(user, id):
+	sendMessage(id, characters[user].stat_string())
 
-	def give_item(item, desc, amount):
-		if item in self.inventory:
-			self.inventory[item][1] += amount
-		else:
-			self.inventory[item] = (desc, amount)
-	
-	def set_stat(stat, num):
-		self.stats[stat] = num
+def show_inventory(user, id):
+	sendMessage(id, characters[user].inventory_string())
 
+def set_stat(user, message, id):
+	message = message.replace("/set_stat ", "", 1)
+	parts = message.split(" ")
+	characters[user].set_stat(parts[0], int(parts[1]))
+	show_stats(user, id)
 
-def roll_stat(stat, user, id):
-	characters{"user"
+def give_item(user, message, id):
+	message = message.replace("/give_item ", "", 1)
+	parts = message.split(" ", 2)
+	print(parts)
+	characters[user].give_item(parts[0], parts[1], parts[2])
+	show_inventory(user, id)
+
+def create_character(message, user, c_id):
+	message = message.replace("/create_character ", "", 1).strip()
+	if message:
+		characters[user] = Character(message)
+		sendMessage(c_id, "Created character " + message + " for player " + user)
+	else:
+		sendMessage(c_id, "Name your character!")
 
 while True:
 	get_updates = json.loads(requests.get(url + 'getUpdates', params=dict(offset=(last_update+1))).text)
@@ -159,7 +213,8 @@ while True:
 		if 'message' in update and 'text' in update['message']:
 			user = update['message']['from']['username']
 			print(str(last_update)+": "+user)
-			message = update['message']['text'].lower()
+			message = update['message']['text']
+			message = message.replace("@"+bot_username, "")
 			c_id = update['message']['chat']['id']
 			if message.startswith('/newtrigger'):
 				parts = message.splitlines()
@@ -183,7 +238,39 @@ while True:
 			elif message.startswith('/roll') or message.startswith("/r"):
 				rollDice(message, c_id)
 			elif message.startswith("/create_character"):
-				createCharacter(message, username, c_id) 
+				create_character(message, user, c_id) 
+			elif message.startswith("/show_stats"):
+				if("@" in message):
+					user = username_pattern.search(message).group(1)
+					message.replace(" @"+user,"",1)
+				if user in characters:
+					show_stats(user, c_id) 
+				else:
+					sendMessage(c_id, user + " does not have a character!")
+			elif message.startswith("/show_inventory"):
+				if("@" in message):
+					user = username_pattern.search(message).group(1)
+					message.replace(" @"+user,"",1)
+				if user in characters:
+					show_inventory(user, c_id) 
+				else:
+					sendMessage(c_id, user + " does not have a character!")
+			elif message.startswith("/set_stat"):
+				if("@" in message):
+					user = username_pattern.search(message).group(1)
+					message.replace(" @"+user,"",1)
+				if user in characters:
+					set_stat(user, message, c_id) 
+				else:
+					sendMessage(c_id, user + " does not have a character!")
+			elif message.startswith("/give_item"):
+				if("@" in message):
+					user = username_pattern.search(message).group(1)
+					message.replace(" @"+user,"",1)
+				if user in characters:
+					give_item(user, message, c_id)
+				else:
+					sendMessage(c_id, user + " does not have a character!")
 			else: # Generic Message
 				for word in phrases:
 					if findWord(word, message):
