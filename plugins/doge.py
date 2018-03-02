8acff5c1c7766bcdd57c540cf165984112d66ecc
Mark Powers <markppowers0@gmail.com>
2018-03-02 12:42:08 -0500

    Update doge

--- plugins/doge.py
+++ plugins/doge.py
@@ -13,63 +13,48 @@ class Doge(Plugin):
 		self.bot = bot
 		self.parts = ["many", "very", "such", "much", "so", "wow"]
 		self.words = []
-		self.colors = []
-		self.coords = []
-
-		self.colors.append((52, 255, 0))  # Green
-		self.colors.append((255, 0, 0))  # Red
-		self.colors.append((0, 0, 255))  # Blue
-		self.colors.append((255, 255, 0))  # Yellow
-		self.colors.append((255, 128, 0))  # Orange
-		self.colors.append((200, 0, 255))  # Purple
-
-		self.coords.append((70, 100))
-		self.coords.append((750, 200))
-		self.coords.append((650, 460))
-		self.coords.append((785, 700))
-		self.coords.append((150, 600))
+		self.colors = [(52, 255, 0), #Green
+					   (255, 0, 0), #Red
+					   (0, 0, 255), # Blue
+					   (255, 255, 0), #Yellow
+					   (255, 128, 0), #Orange
+					   (200, 0, 255)] #Purple
+		self.coords = [(70, 100), (750, 200), (650, 460), (785, 700),(150, 600)]
 
 	def doge_it_up(self, words):
 		try:
-			image = Image.open(self.dir + "/doge_template.jpg")
+			image = Image.open(self.dir + "/doge.jpg")
 			draw = ImageDraw.Draw(image)
 
-			font = ImageFont.truetype("comic.ttf", 64)
-
+			try:
+				font = ImageFont.truetype("comic.ttf", 64)
+			except:
+				font = ImageFont.load_default()
 			random.shuffle(self.colors)
 
 			for word, color, coord in zip(words, self.colors, self.coords):
 				print(word)
 				draw.text(coord, word, color, font)
 
-			image.save(self.dir + "\doge.jpg")
+			image.save(self.dir + "/output.jpg")
 
 		except FileNotFoundError:
-			return "Unable to find file {}/doge_template.jpg".format(self.dir)
+			return "Unable to find file {}/doge.jpg".format(self.dir)
 
 	def on_command(self, command):
 		if command.command == "doge":
-			caption = ""
 			top_words = sorted(self.words, key=len, reverse=True)[:5]
 			random.shuffle(top_words)
 			words_to_draw = []
 
 			for word in top_words:
 				choice = random.choice(self.parts)
-				text = ""
-				if choice == "wow":
-					caption += choice
-					text += choice
-				else:
-					caption += choice + " "+ word
-					text += choice + " "+ word
-				caption += "\n"
-				words_to_draw.append(text)
+				words_to_draw.append(choice if choice == "wow" else choice + " "+ word)
 
 			self.doge_it_up(words_to_draw)
 			self.words = []
-			return  {"type": "photo", "caption": caption.strip(),
-					"file_name": self.dir+"/doge.jpg"}
+			return  {"type": "photo", "caption": "",
+					"file_name": self.dir+"/output.jpg"}
 
 	def get_commands(self):
 		return {"doge"}
