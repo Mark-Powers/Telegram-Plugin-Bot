136c8ab42b555558b9280bf768e7d814885c4473
Matthew Klawitter <mtklaw153@gmail.com>
2019-06-23 15:17:56 -0400

    Implemented ABC to enforce method implementation

--- plugin.py
+++ plugin.py
@@ -1,22 +1,31 @@
+from abc import ABC, abstractmethod
+
 class Plugin:
+
     def __init__(self, data_dir, bot):
         pass
 
+    @abstractmethod
     def on_message(self, message):
         return "This gets called per message when a plugin has access!"
 
+    @abstractmethod
     def on_command(self, command):
         return {"type": "message",
                 "mesasge": "This gets called a command from this plugin is sent!"}
 
+    @abstractmethod
     def get_commands(self):
         return {}
 
+    @abstractmethod
     def get_name(self):
         return "Base Plugin"
 
+    @abstractmethod
     def get_help(self):
         return "This plugin does nothing!"
 
+    @abstractmethod
     def has_message_access(self):
         return False
