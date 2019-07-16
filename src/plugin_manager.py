9dc469e328ad9ea28943f4346f6e74235be28f4f
Matthew Klawitter <mtklaw153@gmail.com>
2019-07-16 19:19:16 -0400

    Added logging to plugin_manager.py

--- src/plugin_manager.py
+++ src/plugin_manager.py
@@ -70,6 +70,8 @@ class PluginManager:
         self.is_enabled = {}
         # Loads Plugins listed within the configuration file
         self.load_plugins(bot)
+        # Reference to custom logger
+        self.logger = logging.getLogger(__name__)
 
     def load_plugins(self, bot):
         """
@@ -92,19 +94,25 @@ class PluginManager:
         self.commands = {}
         self.is_enabled = {}
 
+        self.logger.info("Attempting to load plugins...")
+
         if not self.imported:
             for plugin in self.config_plugins:
                 mod = importlib.import_module("plugins." + plugin, ".")
+                self.logger.info("Importing plugin.{}...".format(plugin))
                 class_ = getattr(mod, "BotPlugin")
                 self.plugins.append(class_(os.getcwd() + "/plugins/{}/".format(plugin), bot))
                 self.modules.append(mod)
+                self.logger.info("Successfully instantiated plugin!")
         else:
-            new_modules =  []
+            new_modules = []
             for module in self.modules:
                 mod = importlib.reload(module)
+                self.logger.info("Attempting to reload plugin module with name {}...".format(module.__name__.split(".")[1]))
                 class_ = getattr(mod, "BotPlugin")
                 self.plugins.append(class_(os.getcwd() + "/plugins/{}/".format(module.__name__.split(".")[1]), bot))
                 new_modules.append(mod)
+                self.logger.info("Successfully reloaded and instantiated plugin!")
             self.modules = new_modules
         
         self.imported = True
@@ -113,8 +121,10 @@ class PluginManager:
             if not plugin.get_commands() == None:
                 for command in plugin.get_commands():
                     self.commands[command] = plugin
+                    self.logger.info("Mapped command ({}) to plugin {}".format(command, plugin.get_name()))
             if plugin.has_message_access():
                 self.message_plugins.append(plugin)
+                self.logger.info("Gave plugin {} message access".format(plugin.get_name()))
             self.is_enabled[plugin.get_name()] = True
 
     def dynamically_load(self, bot):
@@ -145,7 +155,9 @@ class PluginManager:
             The main Bot object responsible for sending and receiving messages
         """
 
+        self.logger.warning("Attempting to reload all accessible plugins...")
         self.load_plugins(bot)
+        self.logger.warning("Reloading complete!")
 
     def process_plugin(self, bot, message):
         """
@@ -165,16 +177,17 @@ class PluginManager:
             Message object detailing command, message, and Telegram user info
         """
 
-        print(message.command.command)
-
         try:
             if message.command.command in self.commands.keys():
                 if self.is_enabled[self.commands[message.command.command].get_name()]:
+                    self.logger.info("Processing command ({}) for plugin ({})".format(message.command.command, self.commands[message.command.command].get_name()))
                     response = self.commands[message.command.command].on_command(message.command)
                 else:
+                    self.logger.warning("Unable to process command {} as it is disabled".format(message.command.command))
                     response =  {"type": "message", "message": "That command is currently disabled or does not exist."}
             else:
-                response =  {"type": "message", "message": "That command is currently disabled or does not exist."}
+                self.logger.warning("Invalid command ({}) received!".format(message.command.command))
+                response =  {"type": "message", "message": "That command is currently disabled or does not exist"}
 
             if response["type"] == "message":
                 bot.send_message(message.chat.id, response["message"])
@@ -225,10 +238,13 @@ class PluginManager:
         for plugin in self.plugins:
             if plugin.get_name() == plugin_name:
                 if not self.is_enabled[plugin.get_name()]:
+                    self.logger.info("Enabling plugin with name ({})".format(plugin.get_name()))
                     self.is_enabled[plugin_name] = True
                     plugin.enable()
                     return True
+                self.logger.warning("Unable to enable plugin with name ({}), it is already enabled".format(plugin.get_name()))
                 return False
+        self.logger.warning("Unable to enable plugin with name ({}), it does not exist!".format(plugin_name)
         return False
 
     def disable_plugin(self, plugin_name):
@@ -248,10 +264,13 @@ class PluginManager:
         for plugin in self.plugins:
             if plugin.get_name() == plugin_name:
                 if self.is_enabled[plugin.get_name()]:
+                    self.logger.info("Disabnling plugin with name ({})".format(plugin.get_name()))
                     self.is_enabled[plugin_name] = False
                     plugin.disable()
                     return True
+                self.logger.warning("Unable to disable plugin with name ({}), it is already disabled".format(plugin.get_name()))
                 return False
+        self.logger.warning("Unable to disable plugin with name ({}), it does not exist!".format(plugin_name)
         return False
 
     def list_commands(self):
